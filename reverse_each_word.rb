ae057423e543204f9cf34c91bcf3cadc8a3a242b
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-28 21:45:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(s)
   new_s = s.split
-  newer_s = []
-  new_s.collect.join(" ") {|i| newer_s << i.reverse}
+  # newer_s = []
+  new_s.collect.join(" ") {|i| i.reverse}
   # end
   # newer_s.join(" ")
 end
