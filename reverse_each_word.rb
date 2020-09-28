8116adeb2fc7c398b19363959aa00515ede33c77
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-28 21:41:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(s)
-  # new_s = s.split
-  # new_s.collect do |i|
-  #   new_s << i.reverse
-  # end
+  new_s = s.split
+  new_s.collect do |i|
+    new_s << i.reverse
+  end
 end
